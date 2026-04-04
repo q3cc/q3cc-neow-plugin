b17a4a5f6da6da32dfca627c406e4a57644bd487
Q3CC <q3cc@q3cc.top>
2026-04-04 08:57:24 +0800

    refactor: rewrite plugin entry for yunzai v0.0.6

---
+++ apps/neow.js
@@ -0,0 +1,91 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import { MeowUserInfoPlugin } from '../meow_user_info.js'
+import { MeowGame24Plugin } from '../meow_game_24.js'
+
+export class NeowPlugin extends plugin {
+  constructor() {
+    super({
+      name: 'neow_plugin',
+      dsc: 'neow 插件总入口',
+      event: 'message',
+      priority: 5000,
+      rule: [
+        {
+          reg: /^\/(?:neowhelp|nhelp)\s*$/i,
+          fnc: 'showHelp'
+        },
+        {
+          reg: /^\/ping\s*$/i,
+          fnc: 'ping'
+        },
+        {
+          reg: /^\/my\s*$/i,
+          fnc: 'myInfo'
+        },
+        {
+          reg: /^\/24g\s+sign\s*$/i,
+          fnc: 'dailySign'
+        },
+        {
+          reg: /^\/24g\s*$/i,
+          fnc: 'showMenu'
+        },
+        {
+          reg: /^\/24g\s+start\s*$/i,
+          fnc: 'startGame'
+        },
+        {
+          reg: /^\/24g\s+difficulty\s*$/i,
+          fnc: 'showDifficultyMenu'
+        },
+        {
+          reg: /^\/24g\s+difficulty\s+(\d+)\s*$/i,
+          fnc: 'setDifficulty'
+        },
+        {
+          reg: /^\/24g\s+answer\s+(.+)$/i,
+          fnc: 'submitAnswer'
+        }
+      ]
+    })
+
+    this.userPlugin = new MeowUserInfoPlugin()
+    this.gamePlugin = new MeowGame24Plugin()
+  }
+
+  async showHelp(e) {
+    return this.userPlugin.showHelp(e)
+  }
+
+  async ping(e) {
+    return this.userPlugin.ping(e)
+  }
+
+  async myInfo(e) {
+    return this.userPlugin.myInfo(e)
+  }
+
+  async dailySign(e) {
+    return this.userPlugin.dailySign(e)
+  }
+
+  async showMenu(e) {
+    return this.gamePlugin.showMenu(e)
+  }
+
+  async startGame(e) {
+    return this.gamePlugin.startGame(e)
+  }
+
+  async showDifficultyMenu(e) {
+    return this.gamePlugin.showDifficultyMenu(e)
+  }
+
+  async setDifficulty(e) {
+    return this.gamePlugin.setDifficulty(e)
+  }
+
+  async submitAnswer(e) {
+    return this.gamePlugin.submitAnswer(e)
+  }
+}
--- index.js
+++ index.js
@@ -1,104 +1,64 @@
-import plugin from '../../lib/plugins/plugin.js'
-import { MeowUserInfoPlugin } from './meow_user_info.js'
-import { MeowGame24Plugin } from './meow_game_24.js'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath, pathToFileURL } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const loggerInstance = globalThis.logger
+
+let pluginVersion = '0.0.6'
+const packagePath = path.join(__dirname, 'package.json')
+
+if (fs.existsSync(packagePath)) {
+  try {
+    pluginVersion = JSON.parse(fs.readFileSync(packagePath, 'utf8')).version || pluginVersion
+  } catch (error) {
+    loggerInstance?.error?.(`[neow] 读取 package.json 失败: ${error.message}`)
+  }
+}
 
 export const Version = Object.freeze({
-  version: 'v0.0.5'
+  version: `v${pluginVersion}`
 })
 
-if (globalThis.Bot?.logger?.info) {
-  globalThis.Bot.logger.info('---------^_^---------')
-  globalThis.Bot.logger.info(`neow插件${Version.version}初始化~`)
+const appDir = path.join(__dirname, 'apps')
+const appFiles = fs.existsSync(appDir)
+  ? fs.readdirSync(appDir).filter(file => file.endsWith('.js'))
+  : []
+
+const results = await Promise.allSettled(
+  appFiles.map(file => import(pathToFileURL(path.join(appDir, file)).href))
+)
+
+const apps = {}
+
+appFiles.forEach((file, index) => {
+  const name = file.replace(/\.js$/i, '')
+  const result = results[index]
+
+  if (result.status !== 'fulfilled') {
+    loggerInstance?.error?.(`[neow] 载入插件文件失败: ${name}`)
+    loggerInstance?.error?.(result.reason)
+    return
+  }
+
+  const mod = result.value
+  const exportKey = Object.keys(mod)[0]
+
+  if (!exportKey) {
+    loggerInstance?.warn?.(`[neow] 插件文件未导出类: ${name}`)
+    return
+  }
+
+  apps[name] = mod[exportKey]
+})
+
+if (loggerInstance?.info) {
+  loggerInstance.info('---------^_^---------')
+  loggerInstance.info(`neow插件${Version.version}初始化~`)
 } else {
+  console.log('---------^_^---------')
   console.log(`neow插件${Version.version}初始化~`)
 }
 
-export class MainPlugin extends plugin {
-  constructor() {
-    super({
-      name: 'neow_plugin',
-      dsc: 'neow 插件总入口',
-      event: 'message',
-      priority: 5000,
-      rule: [
-        {
-          reg: /^\/(?:neowhelp|nhelp)\s*$/i,
-          fnc: 'showHelp'
-        },
-        {
-          reg: /^\/ping\s*$/i,
-          fnc: 'ping'
-        },
-        {
-          reg: /^\/my\s*$/i,
-          fnc: 'myInfo'
-        },
-        {
-          reg: /^\/24g\s+sign\s*$/i,
-          fnc: 'dailySign'
-        },
-        {
-          reg: /^\/24g\s*$/i,
-          fnc: 'showMenu'
-        },
-        {
-          reg: /^\/24g\s+start\s*$/i,
-          fnc: 'startGame'
-        },
-        {
-          reg: /^\/24g\s+difficulty\s*$/i,
-          fnc: 'showDifficultyMenu'
-        },
-        {
-          reg: /^\/24g\s+difficulty\s+(\d+)\s*$/i,
-          fnc: 'setDifficulty'
-        },
-        {
-          reg: /^\/24g\s+answer\s+(.+)$/i,
-          fnc: 'submitAnswer'
-        }
-      ]
-    })
-
-    this.userPlugin = new MeowUserInfoPlugin()
-    this.gamePlugin = new MeowGame24Plugin()
-  }
-
-  async showHelp(e) {
-    return this.userPlugin.showHelp(e)
-  }
-
-  async ping(e) {
-    return this.userPlugin.ping(e)
-  }
-
-  async myInfo(e) {
-    return this.userPlugin.myInfo(e)
-  }
-
-  async dailySign(e) {
-    return this.userPlugin.dailySign(e)
-  }
-
-  async showMenu(e) {
-    return this.gamePlugin.showMenu(e)
-  }
-
-  async startGame(e) {
-    return this.gamePlugin.startGame(e)
-  }
-
-  async showDifficultyMenu(e) {
-    return this.gamePlugin.showDifficultyMenu(e)
-  }
-
-  async setDifficulty(e) {
-    return this.gamePlugin.setDifficulty(e)
-  }
-
-  async submitAnswer(e) {
-    return this.gamePlugin.submitAnswer(e)
-  }
-}
-
-export default MainPlugin
+export { apps }
